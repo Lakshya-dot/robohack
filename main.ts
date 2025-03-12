1da9162577718b833e659941b008f63dc3031e45
Lakshya-dot <lakshyayarlagadda2008@gmail.com>
2025-03-12 22:44:17 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 basic.forever(function () {
-	
+    music.play(music.stringPlayable("G G G G C C C C ", 120), music.PlaybackMode.UntilDone)
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    TPBot.headlightColor(0xff0000)
+    basic.pause(1000)
+    TPBot.headlightColor(0x0000ff)
 })
